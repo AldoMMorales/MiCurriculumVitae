545b3b5b6cce15fded2229b11066a8c68e431086
martinconectado@gmail.com <martinconectado@gmail.com>
2023-01-26 20:34:59 -0300

    funcionalidad con script

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function Onclick () {
-    document.getElementById("datos-pers");
+
+
+
+function init () {
+    var grafica = document.getElementById("sec_grafica");
+    grafica.style.display = "none";
     
+    var mapa = document.getElementById("sec_mapa");
+    mapa.style.display = "none";
     
-    return ("datos-personales");
+    }
+
+function cambio (ref) {
+    ocultar ();
+    var elemento = document.getElementById(ref);
+    elemento.style.display = "block";
     
+}
+
+
+
+function ocultar (){
+    var textos = document.getElementById("sec_textos");
+    textos.style.display = "none";
     
+    var grafica = document.getElementById("sec_grafica");
+    grafica.style.display = "none";
+    
+    var mapa = document.getElementById("sec_mapa");
+    mapa.style.display = "none";
     
 }
